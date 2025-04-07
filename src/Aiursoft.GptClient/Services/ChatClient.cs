4a7ae83fcd17c7631492ce81cf354f7de624000c
Anduin Xue <anduin@aiursoft.com>
2025-04-07 16:59:23 +0000

    Update version to 9.0.22 and enhance logging detail in ChatClient

--- src/Aiursoft.GptClient/Services/ChatClient.cs
+++ src/Aiursoft.GptClient/Services/ChatClient.cs
@@ -60,8 +60,8 @@ public class ChatClient
             responseModel!.FillChoices();
 
             _logger.LogInformation("Asked LLM. Request last question: {0}. Response last answer: {1}. Cost: {2}ms.",
-                model.Messages.LastOrDefault()?.Content?.SafeSubstring(270),
-                responseModel.GetAnswerPart().SafeSubstring(370),
+                model.Messages.LastOrDefault()?.Content?.SafeSubstring(1500),
+                responseModel.GetAnswerPart().SafeSubstring(1500),
                 stopwatch.ElapsedMilliseconds);
             return responseModel;
         }
@@ -70,8 +70,8 @@ public class ChatClient
             var remoteError = await response.Content.ReadAsStringAsync();
 
             _logger.LogError("Ask LLM failed. Request last question: {0}. Response last answer: {1}.",
-                model.Messages.LastOrDefault()?.Content?.SafeSubstring(270),
-                remoteError.SafeSubstring(370));
+                model.Messages.LastOrDefault()?.Content?.SafeSubstring(2000),
+                remoteError.SafeSubstring(2000));
             throw new HttpRequestException(remoteError, raw);
         }
         finally
